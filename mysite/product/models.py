979a46cc837b8216da01cef9eb0a5b3c7964d55d
Татьяна <tanika135@mail.ru>
2024-03-01 17:25:24 +0300

    создана модель lesson

--- mysite/product/models.py
+++ mysite/product/models.py
@@ -6,3 +6,11 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     start_at = models.DateTimeField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
+
+
+class Lesson(models.Model):
+    product = models.OneToOneField(Product, on_delete=models.PROTECT)
+    name = models.CharField(max_length=100)
+    link_to_video = models.CharField(max_length=100)
+
+
