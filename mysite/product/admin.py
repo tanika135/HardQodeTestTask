2d3a08ad744d7a7faf8a05f832fe2ac8e8c4ed49
Татьяна <tanika135@mail.ru>
2024-03-02 01:03:15 +0300

    ProductAccess и Group добавлены в админку

--- mysite/product/admin.py
+++ mysite/product/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Lesson
+from .models import Product, Lesson, ProductAccess, Group
 
 
 @admin.register(Product)
@@ -11,3 +11,12 @@ class ProductAdmin(admin.ModelAdmin):
 class LessonAdmin(admin.ModelAdmin):
     ordering = ['id']
 
+
+@admin.register(ProductAccess)
+class ProductAccessAdmin(admin.ModelAdmin):
+    ordering = ['id']
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    ordering = ['id']
