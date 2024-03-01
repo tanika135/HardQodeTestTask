e30808ac712683a146da65a8a300bb3886ccb258
Татьяна <tanika135@mail.ru>
2024-03-01 17:33:01 +0300

    lesson и product добавлены в админку

--- mysite/product/admin.py
+++ mysite/product/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Product, Lesson
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    ordering = ['id']
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    ordering = ['id']
+
